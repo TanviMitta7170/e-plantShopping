d78bb1018a052ff36849c89c15287af63821a8b7
Tanvi Mitta <mittatanvi1403@gmail.com>
2025-10-05 02:49:49 -0400

    fix: Corrected CartSlice path in ProductList for Vite build

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { addItem } from '../CartSlice';
+import { addItem } from './CartSlice';
 import './ProductList.css'
 import CartItem from './CartItem';
 function ProductList({ onHomeClick }) {
