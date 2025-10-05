dafdada42b63ced66d896ca78d5d000296da5cf5
Tanvi Mitta <mittatanvi1403@gmail.com>
2025-10-05 02:13:42 -0400

    feat: Completed Task 3, CartItem logic (totals, increment, decrement, remove)

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,41 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    return cart.reduce((total, item) => {
+      const unitCost = parseFloat(item.cost.substring(1));
+      return total + item.quantity * unitCost;
+    }, 0).toFixed(2);
   };
 
   const handleContinueShopping = (e) => {
-   
+    e.preventDefault();
+    onContinueShopping(e);
   };
 
-
-
+  const handleCheckoutShopping = (e) => {
+    e.preventDefault();
+    alert('Functionality to be added for future reference');
+  };
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+   if (item.quantity > 1) {
+      dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+    } else {
+      handleRemove(item);
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    const unitCost = parseFloat(item.cost.substring(1));
+    return (item.quantity * unitCost).toFixed(2);
   };
 
   return (
